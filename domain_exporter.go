a06022ac3e9fe088580f9c5a63b4619d3165f1af
Mark Hansen <mark@markhansen.co.nz>
2020-08-24 21:42:25 +1000

    Extract github.com/mhansen/domain

--- domain_exporter.go
+++ domain_exporter.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
 
+	"github.com/mhansen/domain"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	phttp "github.com/travelaudience/go-promhttp"
@@ -41,7 +39,7 @@ func main() {
 		log.Fatalf("could not create http client: %v\n", err)
 	}
 
-	dc := domainClient{c: c}
+	dc := domainCollector{domain.NewClient(c, *apiKey)}
 	reg.MustRegister(
 		prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}),
 		prometheus.NewGoCollector(),
@@ -61,67 +59,11 @@ func main() {
 	}
 }
 
-type domainClient struct {
-	c *http.Client
+type domainCollector struct {
+	*domain.Client
 }
 
-func (dc domainClient) SearchResidentialPage(rsr ResidentialSearchRequest) ([]SearchResult, error) {
-	rsrJSON, err := json.Marshal(rsr)
-	if err != nil {
-		return nil, err
-	}
-	req, err := http.NewRequest("POST", "https://api.domain.com.au/v1/listings/residential/_search", bytes.NewBuffer(rsrJSON))
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Add("X-Api-Key", *apiKey)
-	req.Header.Add("accept", "application/json")
-	log.Printf("making request for page #%v: %v, %+v", rsr.PageNumber, req.URL, rsr)
-	resp, err := dc.c.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("request to %v failed: %v", req.URL.String(), err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		b, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return nil, err
-		}
-		log.Print(string(b))
-		return nil, fmt.Errorf("got non-200 code: %v, %v", resp.StatusCode, resp.Status)
-	}
-
-	listingsPage := []SearchResult{}
-	err = json.NewDecoder(resp.Body).Decode(&listingsPage)
-	if err != nil {
-		return nil, fmt.Errorf("couldn't parse json: %v", err)
-	}
-	log.Printf("got %v listings", len(listingsPage))
-	return listingsPage, nil
-}
-
-func (dc domainClient) SearchResidential(rsr ResidentialSearchRequest) ([]SearchResult, error) {
-	rsr.PageSize = 200
-	rsr.PageNumber = 0
-	listings := []SearchResult{}
-
-	// Domain returns an error: "Cannot page beyond 1000 records" if you try to.
-	for len(listings) < 1000 {
-		listingsPage, err := dc.SearchResidentialPage(rsr)
-		if err != nil {
-			return nil, err
-		}
-		if len(listingsPage) == 0 {
-			break
-		}
-		listings = append(listings, listingsPage...)
-		rsr.PageNumber++
-	}
-	return listings, nil
-}
-
-func (dc domainClient) domainHandler(w http.ResponseWriter, r *http.Request) {
+func (dc domainCollector) domainHandler(w http.ResponseWriter, r *http.Request) {
 	reg := prometheus.NewPedanticRegistry()
 	listingCount := prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
@@ -130,12 +72,12 @@ func (dc domainClient) domainHandler(w http.ResponseWriter, r *http.Request) {
 		[]string{"propertytype", "suburb", "postcode", "bedrooms", "bathrooms", "carspaces"},
 	)
 	reg.MustRegister(listingCount)
-	rsr := ResidentialSearchRequest{
+	rsr := domain.ResidentialSearchRequest{
 		ListingType:  "Rent",
 		MinBathrooms: 0,
 		MinBedrooms:  0,
 		MinCarspaces: 0,
-		Locations: []LocationFilter{
+		Locations: []domain.LocationFilter{
 			{
 				State:                     r.URL.Query().Get("state"),
 				Area:                      "",
@@ -166,42 +108,4 @@ func (dc domainClient) domainHandler(w http.ResponseWriter, r *http.Request) {
 
 	h := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
 	h.ServeHTTP(w, r)
-}
-
-type LocationFilter struct {
-	State                     string `json:"state"`
-	Region                    string `json:"region"`
-	Area                      string `json:"area"`
-	Suburb                    string `json:"suburb"`
-	PostCode                  string `json:"postCode"`
-	IncludeSurroundingSuburbs bool   `json:"includeSurroundingSuburbs"`
-}
-
-type ResidentialSearchRequest struct {
-	ListingType  string `json:"listingType"`
-	MinBedrooms  int    `json:"minBedrooms"`
-	MinBathrooms int    `json:"minBathrooms"`
-	MinCarspaces int    `json:"minCarspaces"`
-	PageSize     int    `json:"pageSize"`
-	PageNumber   int    `json:"pageNumber"`
-	Locations    []LocationFilter
-}
-
-// Listing https://developer.domain.com.au/docs/latest/apis/pkg_agents_listings/references/listings_detailedresidentialsearch
-type SearchResult struct {
-	Listing PropertyListing `json:"listing"`
-}
-
-type PropertyListing struct {
-	PropertyDetails PropertyDetails `json:"propertyDetails"`
-}
-
-type PropertyDetails struct {
-	State        string  `json:"state"`
-	PropertyType string  `json:"propertyType"`
-	Bathrooms    float32 `json:"bathrooms"`
-	Bedrooms     float32 `json:"bedrooms"`
-	CarSpaces    int32   `json:"carspaces"`
-	Suburb       string  `json:"suburb"`
-	Postcode     string  `json:"postcode"`
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/mhansen/domain_exporter
 go 1.14
 
 require (
-	github.com/prometheus/client_golang v1.7.0
+	github.com/mhansen/domain v0.0.0-20200824112302-2529c9791ab8
+	github.com/prometheus/client_golang v1.7.1
+	github.com/prometheus/common v0.13.0 // indirect
 	github.com/travelaudience/go-promhttp v1.0.1
-	golang.org/x/sys v0.0.0-20200620081246-981b61492c35 // indirect
-	google.golang.org/protobuf v1.24.0 // indirect
+	golang.org/x/sys v0.0.0-20200821140526-fda516888d29 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
